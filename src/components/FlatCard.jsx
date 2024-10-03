c4fcf574035d1a2de9d908cbf46c6b799a2f155f
pablormacia <pr.macia@gmail.com>
2024-10-02 23:08:46 -0300

    Clase 6 - Estilos, props y constatntes - P2

---
+++ src/components/FlatCard.jsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native'
+import { colors } from '../global/colors'
+
+const FlatCard = ({children,style}) => {
+  return (
+    <View style={{...styles.cardContainer,...style}}>
+      {children}
+    </View>
+  )
+}
+
+export default FlatCard
+
+const styles = StyleSheet.create({
+    cardContainer:{
+        backgroundColor: colors.grisMedio,
+        shadowColor: colors.negro,
+        shadowOpacity:1,
+        shadowRadius:1,
+        shadowOffset: {width: 3,height:5},
+        elevation:10,
+    }
+})
--- src/screens/CategoriesScreen.jsx
+++ src/screens/CategoriesScreen.jsx
@@ -1,12 +1,28 @@
-import { StyleSheet, Text, View, FlatList } from 'react-native'
+import { StyleSheet, Text, View, FlatList,Image } from 'react-native'
 import categories from "../data/categories.json"
+import FlatCard from '../components/FlatCard'
 
 const CategoriesScreen = () => {
     //console.log("Categories: ", categories)
 
-    const renderCategoryItem = ({item}) =>{
+    const renderCategoryItem = ({item,index}) =>{
+        //console.log(item)
         return(
-            <Text>{item.title}</Text>
+            <FlatCard style={
+                //Uso de operador ternario condicion?si verdadero:si falso
+                index%2==0
+                ?
+                {...styles.categoryItemContainer, ... styles.row}
+                :
+                {...styles.categoryItemContainer, ... styles.rowReverse}
+            }>
+                <Image
+                    source={{uri:item.image }}
+                    style={styles.image}
+                    resizeMode='contain'
+                />
+                <Text style={styles.categoryTitle}>{item.title}</Text>
+            </FlatCard>
         )
     }
 
@@ -24,4 +40,27 @@ const CategoriesScreen = () => {
 
 export default CategoriesScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+    categoryItemContainer: {
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginHorizontal: 10,
+        marginVertical: 5,
+        padding: 20,
+      },
+      categoryTitle: {
+        fontSize: 24,
+        fontWeight: "bold",
+      },
+    image:{
+        width:150,
+        height:80
+    },
+    row:{
+        flexDirection: 'row'
+    },
+    rowReverse:{
+        flexDirection:'row-reverse'
+    }
+})
